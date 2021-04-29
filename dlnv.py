b8ff26a78bc9d2b890bf0b06ae3f0db433b58c47
rileyfroehlich <rileyfro2@gmail.com>
2021-04-29 13:21:48 -0700

    Incorporated main.py script on submit

--- dlnv.py
+++ dlnv.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, Length
 from form import dlnvForm
+from main import check_is_valid
 
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -14,8 +15,27 @@ app.config['SECRET_KEY'] = 'Ftcc9uACqVnPPuuK5EtsUvguUwYIyi0P'
 @app.route("/")
 def validate():
     form = dlnvForm()
+    if form.validate_on_submit():
+ #       if {validation} is not False:
+            flash(f"Your Drivers License Number is {validation}!")
+            return redirect(url_for('submit'))
     return render_template('dlnpage.html', title='Validator', form=form)
 
+@app.route("/submission", methods = ['POST'])
+def submit():
+    fName = request.form['fName']
+    lName = request.form['lName']
+    dlNumber = request.form['dlNumber']
+    state = request.form['state']
+    form = dlnvForm()
+
+    print( check_is_valid(state, dlNumber))
+
+    if form.validate_on_submit():
+#       if {validation} is not False:
+            flash(f"Your Drivers License Number is {validation}!")
+            return redirect(url_for('submit'))
+    return render_template('dlnpage.html', title='Validator', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ main.py
@@ -0,0 +1,85 @@
+from dlnvalidation import is_valid
+
+def get_state_abbr(state):
+    state = str(state)
+
+    us_state_abbrev = {
+        'Alabama': 'AL',
+        'Alaska': 'AK',
+        'American Samoa': 'AS',
+        'Arizona': 'AZ',
+        'Arkansas': 'AR',
+        'California': 'CA',
+        'Colorado': 'CO',
+        'Connecticut': 'CT',
+        'Delaware': 'DE',
+        'District of Columbia': 'DC',
+        'Florida': 'FL',
+        'Georgia': 'GA',
+        'Guam': 'GU',
+        'Hawaii': 'HI',
+        'Idaho': 'ID',
+        'Illinois': 'IL',
+        'Indiana': 'IN',
+        'Iowa': 'IA',
+        'Kansas': 'KS',
+        'Kentucky': 'KY',
+        'Louisiana': 'LA',
+        'Maine': 'ME',
+        'Maryland': 'MD',
+        'Massachusetts': 'MA',
+        'Michigan': 'MI',
+        'Minnesota': 'MN',
+        'Mississippi': 'MS',
+        'Missouri': 'MO',
+        'Montana': 'MT',
+        'Nebraska': 'NE',
+        'Nevada': 'NV',
+        'New Hampshire': 'NH',
+        'New Jersey': 'NJ',
+        'New Mexico': 'NM',
+        'New York': 'NY',
+        'North Carolina': 'NC',
+        'North Dakota': 'ND',
+        'Northern Mariana Islands':'MP',
+        'Ohio': 'OH',
+        'Oklahoma': 'OK',
+        'Oregon': 'OR',
+        'Pennsylvania': 'PA',
+        'Puerto Rico': 'PR',
+        'Rhode Island': 'RI',
+        'South Carolina': 'SC',
+        'South Dakota': 'SD',
+        'Tennessee': 'TN',
+        'Texas': 'TX',
+        'Utah': 'UT',
+        'Vermont': 'VT',
+        'Virgin Islands': 'VI',
+        'Virginia': 'VA',
+        'Washington': 'WA',
+        'West Virginia': 'WV',
+        'Wisconsin': 'WI',
+        'Wyoming': 'WY'
+    }
+
+    if len(state) == 2:
+        return state.upper()
+
+    state = state[0].upper() + state[1:].lower()
+
+    if state in us_state_abbrev.keys():
+        return us_state_abbrev[state]
+    else:
+        return None
+
+def check_is_valid(state_abbr, dln):
+    if state_abbr is not None:
+        try:
+            if is_valid(dln, state_abbr):
+                return "Your Driver's License # is Valid"
+            else:
+                return "Your Driver's License # is not Valid"
+        except Exception as e:
+            return str(e)
+    else:
+        return "Invalid State"
