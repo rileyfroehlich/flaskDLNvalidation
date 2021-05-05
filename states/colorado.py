33e3b9d63d3dbf41464f4419463403ead50a7685
rileyfroehlich <rileyfro2@gmail.com>
2021-05-05 12:21:57 -0700

    Minor bug in colorado, basic comments

--- states/colorado.py
+++ states/colorado.py
@@ -1,12 +1,20 @@
 from datetime import date, datetime
 
+'''
+Colorado uses the last 2 digits of year, if before 92' it becomes 1992
+then the date of the year issued eg. Jan 1 = 001/ Dec 31 = 365
+The last 4 digits are sequential to the license issued that day
+at that DMV
+'''
+
 def get_num(day, month, year):
     sdx = ''
 
     expiration_date = str(year) + '-' + str(month) + '-' + str(day)
     day_of_year = datetime.strptime(expiration_date,"%Y-%m-%d").date().strftime('%j')
-    print(day_of_year)
 
+    if int(year) <= 1992:
+        year = 1992
     sdx += str(year)[2:]
     sdx += str(day_of_year)
 
